583246d8c6b396c4e6659276ea27b0327e70fc12
vitor-ferr <vitolemos55@gmail.com>
2023-12-05 00:57:39 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log ("eaw")
+let nome = "Vitor"
+let xp = 6005
+
+if (xp < 1000) {
+    console.log ("Ferro");
+} else if (xp>=1001 && xp<=2000) {
+    console.log ("Bronze");
+} else if (xp>=2001 && xp<=5000) {
+    console.log ("Prata");
+} else if (xp>=6001 && xp<=7000) {
+    console.log ("Ouro");
+} else if (xp>=7001 && xp<=8000) {
+    console.log ("Platina")
+} else if (xp>=8001 && xp <=9000) {
+    console.log ("Ascendente")
+} else if (xp>=9001 && xp <= 10000) {
+    console.log ("Imortal")
+} else if (xp>=10001) {
+    console.log ("Radiante")
+}
+
+console.log ('O Herói de nome' + nome + 'está no nivel' + xp)
